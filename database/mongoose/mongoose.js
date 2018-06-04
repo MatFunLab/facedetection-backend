5b2c3f4fb0be53f5f72a31370544c4a7855a08a0
MatFunLab <matfunlab@gmail.com>
2018-06-05 01:40:59 +0200

    fixing MONGODB_URI

--- database/mongoose/mongoose.js
+++ database/mongoose/mongoose.js
@@ -1,6 +1,10 @@
 const mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost:27017/facedetection");
 
+if(process.env.PORT === 3000) {
+mongoose.connect("mongodb://localhost:27017/facedetection");
+}
+
+mongoose.connect("mongodb://heroku_g3hlj416:n577otl6rci8nmjho0i16h3lqr@ds147890.mlab.com:47890/heroku_g3hlj416");
 module.exports = {mongoose};
