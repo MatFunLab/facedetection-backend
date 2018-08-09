ee946e49f266b2dac2f6560dc5fa593f218c7843
MatFunLab <matfunlab@gmail.com>
2018-08-09 08:53:17 +0200

    setting up ENV variable in Heroku

--- database/mongoose/mongoose.js
+++ database/mongoose/mongoose.js
@@ -2,9 +2,12 @@ const mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise;
 
-if(process.env.PORT === 3000) {
-mongoose.connect("mongodb://localhost:27017/facedetection");
+// if (process.env.PORT === 3000) {
+// mongoose.connect("mongodb://localhost:27017/facedetection");
+// }
+  
 }
 
-mongoose.connect("mongodb://heroku_g3hlj416:n577otl6rci8nmjho0i16h3lqr@ds147890.mlab.com:47890/heroku_g3hlj416");
+mongoose.connect(process.env.MONGODB_URI);
+
 module.exports = {mongoose};
